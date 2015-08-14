073b7bae7e44df6cec58654b6e038171b7a84b8f
Ranjith Raj D <ranjith.raj@imaginea.com>
2015-08-14 19:06:20 +0530

    JSON Response using greeting vo, Added jackson parser

--- springfire/src/main/java/com/ran/sample/spring/controller/HealthCheck.java
+++ springfire/src/main/java/com/ran/sample/spring/controller/HealthCheck.java
@@ -1,15 +1,26 @@
 package com.ran.sample.spring.controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ran.sample.spring.vo.Greeting;
 
 @RestController
 @RequestMapping("/healthCheck")
 public class HealthCheck {
-
+		//Return simple String	
 		@RequestMapping(value = "/default", method = RequestMethod.GET)
-		public String hello(ModelMap model) {			
+		public String hello() {
+		
 			return "I am fine";
-		}				
+			
+		}		
+				
+		@RequestMapping(value = "/greet/{msg}", method = RequestMethod.GET)
+		public @ResponseBody Greeting displayMessage(@PathVariable String msg) {			
+			return new Greeting(1, msg + "!") ;
+		}
 	}
