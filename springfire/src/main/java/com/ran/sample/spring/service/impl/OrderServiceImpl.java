c4d6cc0997429d016348a3f123edade824f1dcdf
Ranjith Raj D <ranjith.raj@imaginea.com>
2015-08-14 22:58:06 +0530

    xml file loading from classpath using bean def

--- springfire/src/main/java/com/ran/sample/spring/service/impl/OrderServiceImpl.java
+++ springfire/src/main/java/com/ran/sample/spring/service/impl/OrderServiceImpl.java
@@ -1,26 +1,28 @@
 package com.ran.sample.spring.service.impl;
 
+import java.io.InputStream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import com.ran.sample.spring.model.OrderFactory;
 import com.ran.sample.spring.model.Orders;
-import com.ran.sample.spring.model.Orders.Order;
 import com.ran.sample.spring.service.OrderService;
+import com.ran.sample.spring.utils.XMLParser;
 
 @Service
 public class OrderServiceImpl implements OrderService {
-
+	
+	@Autowired
+	@Qualifier(value="contents")
+	private InputStream contents;
+	
+	@Autowired
+	private XMLParser xmlParser;
+	
 	@Override
-	public Orders getAllOrders() {
-		
-		OrderFactory of=new OrderFactory();
-		
-		Orders orders=of.createOrders();
-			Order order=new Order();
-			order.setOrderId(11);
-			order.setTransactions(1);	 
-		orders.setOrder(order);
-		
+	public Orders getAllOrders() {						
+		Orders orders=xmlParser.readObject(contents);		 
 		return orders;
 	}
 
