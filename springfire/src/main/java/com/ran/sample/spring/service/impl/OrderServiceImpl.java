6620ddd8cf3ec71c072383f66d754c5e0a4deefe
Ranjith Raj D <ranjith.raj@imaginea.com>
2015-08-15 20:33:46 +0530

    REST security implemented with spring default login page

--- springfire/src/main/java/com/ran/sample/spring/service/impl/OrderServiceImpl.java
+++ springfire/src/main/java/com/ran/sample/spring/service/impl/OrderServiceImpl.java
@@ -1,28 +1,24 @@
 package com.ran.sample.spring.service.impl;
 
-import java.io.InputStream;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import com.ran.sample.spring.model.OrderFactory;
 import com.ran.sample.spring.model.Orders;
+import com.ran.sample.spring.model.Orders.Order;
 import com.ran.sample.spring.service.OrderService;
-import com.ran.sample.spring.utils.XMLParser;
 
 @Service
 public class OrderServiceImpl implements OrderService {
-	
-	@Autowired
-	@Qualifier(value="contents")
-	private InputStream contents;
-	
-	@Autowired
-	private XMLParser xmlParser;
-	
+		
 	@Override
 	public Orders getAllOrders() {						
-		Orders orders=xmlParser.readObject(contents);		 
+		Orders orders=new OrderFactory().createOrders();
+		orders.setCmd("First");
+		orders.setEnv("1");
+		Order order=new Order();
+		order.setOrderId(101);
+		order.setTransactions(1);
+		orders.setOrder(order);
 		return orders;
 	}
 
