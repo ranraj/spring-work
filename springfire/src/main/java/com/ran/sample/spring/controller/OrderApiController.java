74b6282539f6c25eb7124755b7a11d3fded90a81
Ranjith Raj D <ranjith.raj@imaginea.com>
2015-08-15 10:06:45 +0530

    spring multipart upload in order api

--- springfire/src/main/java/com/ran/sample/spring/controller/OrderApiController.java
+++ springfire/src/main/java/com/ran/sample/spring/controller/OrderApiController.java
@@ -1,8 +1,15 @@
 package com.ran.sample.spring.controller;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ran.sample.spring.model.Orders;
 import com.ran.sample.spring.service.impl.OrderServiceImpl;
@@ -19,11 +26,23 @@ public class OrderApiController {
 		public Orders getAllOrder() {			
 			return orderService.getAllOrders();
 		}
-		
-		public OrderServiceImpl getOrderService() {
-			return orderService;
-		}
-		public void setOrderService(OrderServiceImpl orderService) {
-			this.orderService = orderService;
-		}
+		@RequestMapping(value="/upload", method=RequestMethod.POST )
+	    public @ResponseBody String singleSave(@RequestParam("file") MultipartFile file, @RequestParam("desc") String desc ){	    	
+	    	String fileName = null;
+	    	if (!file.isEmpty()) {
+	            try {
+	                fileName = file.getOriginalFilename();
+	                byte[] bytes = file.getBytes();
+	                BufferedOutputStream buffStream = 
+	                        new BufferedOutputStream(new FileOutputStream(new File(fileName)));
+	                buffStream.write(bytes);
+	                buffStream.close();
+	                return "You have successfully uploaded " + fileName;
+	            } catch (Exception e) {
+	                return "You failed to upload " + fileName + ": " + e.getMessage();
+	            }
+	        } else {
+	            return "Unable to upload. File is empty.";
+	        }
+	    }		
 	}
