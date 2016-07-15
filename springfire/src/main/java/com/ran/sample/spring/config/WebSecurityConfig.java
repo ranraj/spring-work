fcf5d0994874418f7fccdd985c8e475449a557ee
Ranjith Raj D <ranjith.raj@imaginea.com>
2016-07-15 18:25:05 +0530

    REST security implemented with spring default login page

---
+++ springfire/src/main/java/com/ran/sample/spring/config/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.ran.sample.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/order/**").access("hasRole('ROLE_ADMIN')").and().formLogin();
+    }
+
+    @Configuration
+    protected static class AuthenticationConfiguration extends GlobalAuthenticationConfigurerAdapter {
+        @Override
+        public void init(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication().withUser("user").password("user123").roles("USER");
+            auth.inMemoryAuthentication().withUser("admin").password("admin123").roles("ADMIN");
+        }
+    }
+}
