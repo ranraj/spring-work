d9319a3c560ab20cd9e832882662f1c37fb0a335
Ranjith Raj D <ranjith.raj@imaginea.com>
2016-07-15 18:18:15 +0530

    spring multipart upload in order api

--- springfire/src/main/java/com/ran/sample/spring/controller/OrderApiController.java
+++ springfire/src/main/java/com/ran/sample/spring/controller/OrderApiController.java
@@ -6,8 +6,14 @@ import com.ran.sample.spring.service.impl.OrderServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 
 @RestController
@@ -21,6 +27,25 @@ public class OrderApiController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public List<Order> getAllOrder() {
         return orderService.getAllOrders();
+    }
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public @ResponseBody String singleSave(@RequestParam("file") MultipartFile file, @RequestParam("desc") String desc) {
+        String fileName = null;
+        if (!file.isEmpty()) {
+            try {
+                fileName = file.getOriginalFilename();
+                byte[] bytes = file.getBytes();
+                BufferedOutputStream buffStream = new BufferedOutputStream(new FileOutputStream(new File(fileName)));
+                buffStream.write(bytes);
+                buffStream.close();
+                return "You have successfully uploaded " + fileName;
+            } catch (Exception e) {
+                return "You failed to upload " + fileName + ": " + e.getMessage();
+            }
+        } else {
+            return "Unable to upload. File is empty.";
+        }
     }
 
     public OrderServiceImpl getOrderService() {
